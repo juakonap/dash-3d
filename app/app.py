4cce0b30bcdca74c9074bb5b600384f10519903a
juakonap <juakonap@gmail.com>
2021-02-02 06:46:50 -0300

    MODIFY: 3d-plots examples

--- app/app.py
+++ app/app.py
@@ -2,34 +2,50 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import plotly.express as px
 
-df = px.data.iris()
+import plotly.graph_objects as go
+import pandas as pd
+
+
+# Read data from a csv
+z_data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
+
+fig = go.Figure(data=[go.Surface(z=z_data.values)])
+fig.update_traces(contours_z=dict(show=True, usecolormap=True,
+                                  highlightcolor="limegreen", project_z=True))
+fig.update_layout(title='Mt Bruno Elevation', autosize=False,
+                  scene_camera_eye=dict(x=1.87, y=0.88, z=-0.64),
+                  width=500, height=500,
+                  margin=dict(l=65, r=50, b=65, t=90))
+
+
+cont = go.Figure(go.Surface(
+    contours = {
+        "x": {"show": True, "start": 1.5, "end": 2, "size": 0.04, "color":"white"},
+        "z": {"show": True, "start": 0.5, "end": 0.8, "size": 0.05}
+    },
+    x = [1,2,3,4,5],
+    y = [1,2,3,4,5],
+    z = [
+        [0, 1, 0, 1, 0],
+        [1, 0, 1, 0, 1],
+        [0, 1, 0, 1, 0],
+        [1, 0, 1, 0, 1],
+        [0, 1, 0, 1, 0]
+    ]))
+cont.update_layout(
+        scene = {
+            "xaxis": {"nticks": 20},
+            "zaxis": {"nticks": 4},
+            'camera_eye': {"x": 0, "y": -1, "z": 0.5},
+            "aspectratio": {"x": 1, "y": 1, "z": 0.2}
+        })
+
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    dcc.Graph(id="scatter-plot"),
-    html.P("Petal Width:"),
-    dcc.RangeSlider(
-        id='range-slider',
-        min=0, max=2.5, step=0.1,
-        marks={0: '0', 2.5: '2.5'},
-        value=[0.5, 2]
-    ),
-])
-
-@app.callback(
-    Output("scatter-plot", "figure"), 
-    [Input("range-slider", "value")])
-def update_bar_chart(slider_range):
-    low, high = slider_range
-    mask = (df.petal_width > low) & (df.petal_width < high)
-
-    fig = px.scatter_3d(df[mask], 
-        x='sepal_length', y='sepal_width', z='petal_width',
-        color="species", hover_data=['petal_width'])
-    return fig
-app.run_server(debug=True)
-
-
+    dcc.Graph(id="3d-surface-plot", figure=fig),
+    dcc.Graph(id="contour-surface-plot", figure=cont)
+    ],
+    style={'columnCount': 2})
